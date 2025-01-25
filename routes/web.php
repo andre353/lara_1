3152e65de3295ff76a1fb84ae4d23e2b8cbe43cb
andre353 <eva595eva@gmail.com>
2025-01-25 20:52:50 +0300

    remove skills array duplicate, access it via closure

--- routes/web.php
+++ routes/web.php
@@ -3,33 +3,37 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+
+$skills = [
+    [
+        'id' => 1,
+        'name' => 'Технический писатель',
+        'experience' => '> 7 лет',
+    ],
+    [
+        'id' => 2,
+        'name' => 'Переговоры',
+        'experience' => '> 10 лет',
+    ],
+    [
+        'id' => 3,
+        'name' => 'Разработка',
+        'experience' => '1,5 года',
+    ],
+    [
+        'id' => 4,
+        'name' => 'UI/UX',
+        'experience' => '0,5 года',
+    ]
+];
+
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/skills/{id}', function ($id) {
-    $skills = [
-        [
-            'id' => 1,
-            'name' => 'Технический писатель',
-            'experience' => '> 7 лет',
-        ],
-        [
-            'id' => 2,
-            'name' => 'Переговоры',
-            'experience' => '> 10 лет',
-        ],
-        [
-            'id' => 3,
-            'name' => 'Разработка',
-            'experience' => '1,5 года',
-        ],
-        [
-            'id' => 4,
-            'name' => 'UI/UX',
-            'experience' => '0,5 года',
-        ]
-    ];
+Route::get('/skills/{id}', function ($id) use($skills) {
+    
 
     // \Illuminate\Support\Arr::first($skills, function ($skill) use ($id) {
     //     return $skill['id'] == $id;
@@ -41,30 +45,9 @@ Route::get('/skills/{id}', function ($id) {
     return view('skill', ['skill' => $skill]);    
 });
 
-Route::get('/skills', function () {
+Route::get('/skills', function () use($skills) {
     return view('skills', [
-        'skills' => [
-            [
-                'id' => 1,
-                'name' => 'Технический писатель',
-                'experience' => '> 7 лет',
-            ],
-            [
-                'id' => 2,
-                'name' => 'Переговоры',
-                'experience' => '> 10 лет',
-            ],
-            [
-                'id' => 3,
-                'name' => 'Разработка',
-                'experience' => '1,5 года',
-            ],
-            [
-                'id' => 4,
-                'name' => 'UI/UX',
-                'experience' => '0,5 года',
-            ]
-        ]
+        'skills' => $skills
     ]);
 });
 
