e3624022e3eecd83082bc3dee74e1b07193cf73f
andre353 <eva595eva@gmail.com>
2025-01-29 13:26:49 +0300

    add styles to jobs li elements

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,12 +2,15 @@
     <x-slot:heading>
         Вакансии
     </x-slot:heading>
-    <ul>
+    <ul class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-gray-800 hover:underline">
-                    <strong>{{ $job['title'] }}</strong>, зарплата: <strong>{{ $job['salary']}}</strong> руб.
-                </a>
+            <li class="border border-gray-200 rounded-lg">
+                <article>
+                    <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 text-gray-800 hover:underline">
+                        <span class="block font-bold text-blue-500 text-sm">{{ $job->employer->name }}</span>
+                        <span><strong>{{ $job['title'] }}</strong>, зарплата: <strong>{{ $job['salary']}}</strong> руб.</span>
+                    </a>
+                </article>
             </li>
         @endforeach
     </ul>
