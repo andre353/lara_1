e2a1421e03659455951ff9cc3125bca3c609737c
andre353 <eva595eva@gmail.com>
2025-01-29 17:25:09 +0300

    tweak jobs view folder, add create job route

---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        Create Job
+    </x-slot:heading>
+    <p>
+        TODO
+    </p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,19 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/jobs/create', function () {       
+    return view('jobs.create');    
+});
+
 Route::get('/jobs/{id}', function ($id) {    
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);    
+    return view('jobs.show', ['job' => $job]);    
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => Job::with('employer')->paginate(3),
     ]);
 });
