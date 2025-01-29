7d55711abbe0df4c7a5b0a0364cc8cddb4f5f278
andre353 <eva595eva@gmail.com>
2025-01-29 18:56:56 +0300

    add job create form and route, add model unguard for dev mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 // use Illuminate\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
         // Paginator::defaultView('pagination::default');
         // Paginator::defaultView('pagination::simple-bootstrap-4');
         // Paginator::defaultView('pagination::bootstrap-5');
+        Model::unguard();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,36 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs/create', function () {       
-    return view('jobs.create');    
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
-Route::get('/jobs/{id}', function ($id) {    
+Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('jobs.show', ['job' => $job]);    
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/jobs', function () {
     return view('jobs.index', [
-        'jobs' => Job::with('employer')->paginate(3),
+        'jobs' => Job::with('employer')->latest()->paginate(3),
     ]);
 });
+
+Route::post('/jobs', function () {
+    // validation
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+
 
 Route::get('/contact', function () {
     return view('contact');
