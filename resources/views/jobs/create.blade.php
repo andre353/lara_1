774990c48e788b50605d8bb00b0a8f8949e17679
andre353 <eva595eva@gmail.com>
2025-01-31 21:32:16 +0300

    add register and login views, controllers and routes, add form button component

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -17,28 +17,20 @@
                             <x-form-error name="title"/>
                         </div>
                     </x-form-field>
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Зарплата</label>
+                    <x-form-field>
+                        <x-form-label for="salary">Зарплата</x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="text" name="salary" id="salary"
-                                    class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="$2000" required>
-                            </div>
+                            <x-form-input type="text" name="salary" id="salary" placeholder="100000" required/>
+                            <x-form-error name="salary"/>
                         </div>
-                        @error('salary')
-                            <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-                        @enderror
-                    </div>
+                    </x-form-field>
                 </div>       
             </div>
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <x-form-button>Отправить</x-form-button>
         </div>
     </form>
 
