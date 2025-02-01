9f9ef9a0bf0000cc2f1fbbcf8b4b866af5b31927
andre353 <eva595eva@gmail.com>
2025-02-01 14:10:32 +0300

    add store action for the register route, add first_name and last_name fields to user fillable

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -12,6 +14,27 @@ class RegisteredUserController extends Controller
     }
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $validatedAttribures = request()->validate([
+            'first_name' => ['required','string','min:3'],
+            'last_name' => ['required','string','min:3'],
+            'email' => ['required','email','max:254'],
+            // 'email_verified' => ['nullable','date'],
+            'password' => ['required', Password::min(6)->letters()->numbers()->max(6), 'confirmed'], // input: password_confirmation
+        ]);
+
+        // create user
+        $user = User::create($validatedAttribures);
+        // User::create([
+        //     'first_name'=> request('first_name'),
+        //     'last_name'=> request('last_name'),
+        //     'email'=> request('email'),
+        //     'password'=> bcrypt(request('password')),
+        // ]);
+
+        // log in
+        Auth::login($user);
+        // redirect
+        return redirect('/jobs');
     }
 }
